a026d387728b55f768f4c0a2546e9e0e2e17ec45
Jansoon <ckdals4600@naver.com>
2024-01-10 23:07:27 +0900

    :tada: INIT: Init Project

---
+++ src/main/java/forus/naviforyou/NaviforyouApplication.java
@@ -0,0 +1,13 @@
+package forus.naviforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaviforyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaviforyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/forus/naviforyou/NaviforyouApplicationTests.java
@@ -0,0 +1,13 @@
+package forus.naviforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviforyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
