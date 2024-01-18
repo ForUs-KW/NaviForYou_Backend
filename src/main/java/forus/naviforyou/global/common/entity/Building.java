a8eeb4a60b880d84999020081acd93b431915668
sohyun-99 <sophia4130@naver.com>
2024-01-18 21:11:04 +0900

    :bug: Fix:  Building 엔티티 수정 (#6)

--- src/main/java/forus/naviforyou/global/common/entity/Building.java
+++ src/main/java/forus/naviforyou/global/common/entity/Building.java
@@ -1,43 +1,33 @@
 package forus.naviforyou.global.common.entity;
 
-import forus.naviforyou.global.common.BaseEntity;
-import javax.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.BatchSize;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Getter
+@Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Building extends BaseEntity {
+@Document(collection = "buildings")
+public class Building {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
     private String name;
-
-    @Column(name = "general_address")
     private String generalAddress;
-
-    @Column(name = "road_address")
     private String roadAddress;
-
-    @Column(name = "detail_address")
     private String detailAddress;
-
-    @Column(name = "pos_x")
     private Float posX;
-
-    @Column(name = "pos_y")
     private Float posY;
 
-    @BatchSize(size = 100)
+    @DBRef
     @Builder.Default
-    @OneToMany(mappedBy = "building", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<CustomizedAccessibility> customizedAccessibilityList = new ArrayList<>();
 
 }
+
