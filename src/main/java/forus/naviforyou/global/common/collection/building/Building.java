f6afbb121cdce18a746261737f87d72c2e5c6bf3
sohyun-99 <sophia4130@naver.com>
2024-01-19 02:17:55 +0900

    :recycle: Refactoring: Building class 구조 변경 (#6)

--- src/main/java/forus/naviforyou/global/common/collection/building/Building.java
+++ src/main/java/forus/naviforyou/global/common/collection/building/Building.java
@@ -1,5 +1,6 @@
-package forus.naviforyou.global.common.entity;
+package forus.naviforyou.global.common.collection.building;
 
+import forus.naviforyou.global.common.collection.enums.Accessibility;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -19,15 +20,19 @@ public class Building {
     private String id;
 
     private String name;
-    private String generalAddress;
-    private String roadAddress;
-    private String detailAddress;
-    private Float posX;
-    private Float posY;
 
-    @DBRef
-    @Builder.Default
-    private List<CustomizedAccessibility> customizedAccessibilityList = new ArrayList<>();
+    private String generalAddress;
+
+    private String roadAddress;
+
+    private String detailAddress;
+
+    private Location location ;
+
+    private List<Accessibility> accessibilityList ; // 보유한 장애물 리스트
+
+    private List<UserReport> userReportList ; // 유저가 추가한 {장애물,count} 리스트
+
 
 }
 
---
+++ src/main/java/forus/naviforyou/global/common/collection/building/Location.java
@@ -0,0 +1,6 @@
+package forus.naviforyou.global.common.collection.building;
+
+public class Location {
+    private Float posX;
+    private Float posY;
+}
