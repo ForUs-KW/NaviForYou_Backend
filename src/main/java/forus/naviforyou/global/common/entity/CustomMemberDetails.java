95670fc62d108a78df3f1bf0efd0a87e69affdec
jansoon <ckdals4600@naver.com>
2024-01-18 20:19:44 +0900

    :sparkles: Feat: MemberDetails 추가 (#5)

---
+++ src/main/java/forus/naviforyou/global/common/entity/CustomMemberDetails.java
@@ -0,0 +1,48 @@
+package forus.naviforyou.global.common.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomMemberDetails implements UserDetails {
+
+    private Member member;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(() -> member.getRole().toString());
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
