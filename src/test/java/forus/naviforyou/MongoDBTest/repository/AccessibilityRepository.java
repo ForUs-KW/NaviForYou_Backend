6ef1b37ed074f31a8c07c5f442650ede6efb8f5c
sohyun-99 <61389446+sohyun-99@users.noreply.github.com>
2024-01-19 21:54:22 +0900

    🔥Remove : AccessibilityRepository 삭제

--- src/test/java/forus/naviforyou/MongoDBTest/repository/AccessibilityRepository.java
+++
@@ -1,8 +0,0 @@
-package forus.naviforyou.MongoDBTest.repository;
-
-import forus.naviforyou.global.common.collection.Accessibility;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccessibilityRepository extends MongoRepository<Accessibility, String> {
-    // 여기에 추가적인 메서드를 선언하여 사용할 수 있습니다.
-}
