e0eb1c288549c46ad62d408b32fb06f6735c52e2
jansoon <ckdals4600@naver.com>
2024-01-19 18:15:25 +0900

    :bug: Fix: @AuthenticationPrincipal null 값 오류 수정  (#5)

--- src/main/java/forus/naviforyou/global/common/collection/member/Member.java
+++ src/main/java/forus/naviforyou/global/common/collection/member/Member.java
@@ -5,18 +5,22 @@ import forus.naviforyou.global.common.collection.enums.MemberType;
 import forus.naviforyou.global.common.collection.enums.Role;
 import lombok.*;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "members")
-public class Member {
+public class Member implements UserDetails {
     @Id
     private String id;
 
@@ -34,7 +38,41 @@ public class Member {
     @DBRef
     private List<Alter> alterList ;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = List.of(new SimpleGrantedAuthority(role.toString()));
+        return grantedAuthorities;
+    }
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
 
