eb020a2948b26636e861285fee71d80c19e9b20f
jansoon <ckdals4600@naver.com>
2024-01-20 13:41:18 +0900

    :memo: Docs: PERMIT_URIS에 "/app/member/kakao" 추가 (#11)

--- src/main/java/forus/naviforyou/global/common/Constants.java
+++ src/main/java/forus/naviforyou/global/common/Constants.java
@@ -2,6 +2,6 @@ package forus.naviforyou.global.common;
 
 
 public class Constants {
-    public static final String[] PERMIT_URIS = {"/app/member/login", "/app/member/signUp"};
+    public static final String[] PERMIT_URIS = {"/app/member/login", "/app/member/signUp", "/app/member/kakao"};
     public static final String JWT_AUTHORITIES_KEY = "auth";
 }
