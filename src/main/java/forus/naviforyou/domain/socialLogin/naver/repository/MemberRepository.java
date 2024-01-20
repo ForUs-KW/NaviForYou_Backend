19b575f259ef7498e408191a331713f0260fcfbf
sohyun-99 <sophia4130@naver.com>
2024-01-20 22:07:37 +0900

    :fire: Remove : 테스트 코드 삭제

---
+++ src/main/java/forus/naviforyou/domain/socialLogin/naver/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+//package forus.naviforyou.domain.socialLogin.naver.repository;
+//
+//import forus.naviforyou.global.common.collection.member.Member;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface MemberRepository extends MongoRepository<Member,String> {
+//    Optional<Member> findByEmail(String email);
+//
+//}
