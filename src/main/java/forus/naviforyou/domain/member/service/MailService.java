09ee1fb2b25f260beed1c3ab7845ce37ed40dc5c
Jansoon <ckdals4600@naver.com>
2024-01-21 04:18:12 +0900

    :sparkles: Feat: 이메일 전송 기능 추가 (#15)

---
+++ src/main/java/forus/naviforyou/domain/member/service/MailService.java
@@ -0,0 +1,37 @@
+package forus.naviforyou.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    @Value("${mail.username}")
+    private String sender;
+    private final JavaMailSender mailSender;
+
+    public void mailSend(String toMail, String title, String content) {
+        MimeMessage message = mailSender.createMimeMessage();//JavaMailSender 객체를 사용하여 MimeMessage 객체를 생성
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");
+            helper.setFrom(sender);
+            helper.setTo(toMail);
+            helper.setSubject(title);
+            helper.setText(content,true);
+            mailSender.send(message);
+        } catch (MessagingException e) {//이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
+
+            e.printStackTrace();
+        }
+
+
+    }
+}
