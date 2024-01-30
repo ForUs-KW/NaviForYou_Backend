e429cdc7dd0bc8091a2cc04b40a01eb585d88b41
jansoon <ckdals4600@naver.com>
2024-01-31 03:46:15 +0900

    :bug: Fix: Repository 상속 오류 수정 (#30) - JpaRepository -> MongoRepository

--- src/main/java/forus/naviforyou/domain/place/repository/BuildingRepository.java
+++ src/main/java/forus/naviforyou/domain/place/repository/BuildingRepository.java
@@ -3,12 +3,12 @@ package forus.naviforyou.domain.place.repository;
 
 import forus.naviforyou.global.common.collection.building.Building;
 import forus.naviforyou.global.common.collection.building.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface BuildingRepository extends JpaRepository<Building, Long> {
+public interface BuildingRepository extends MongoRepository<Building, Long> {
     Optional<Building> findByLocation(Location location);
 }
