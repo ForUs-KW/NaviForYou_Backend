996665838022e8af75b7e7bee4459fedfe26ab5f
jansoon <ckdals4600@naver.com>
2024-01-31 03:08:24 +0900

    :sparkles: Feat: 기본 응답 DTO 추가 (#30)

---
+++ src/main/java/forus/naviforyou/global/common/BaseResultRes.java
@@ -0,0 +1,12 @@
+package forus.naviforyou.global.common;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class BaseResultRes {
+    private Boolean result;
+
+}
