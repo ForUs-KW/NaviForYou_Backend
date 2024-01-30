023ccc36242253d80035e6b02a26131b4251b74d
jansoon <ckdals4600@naver.com>
2024-01-31 04:25:36 +0900

    :bulb: Style: 주석 변경 (#30)

--- src/main/java/forus/naviforyou/global/common/collection/enums/Accessibility.java
+++ src/main/java/forus/naviforyou/global/common/collection/enums/Accessibility.java
@@ -3,4 +3,14 @@ package forus.naviforyou.global.common.collection.enums;
 public enum Accessibility {
     BUMP , ELEVATOR , SLIDE , TOILET, PARKING
     // 방지턱  , 엘리베이터 , 들어갈때 오르막길 제공 , 화장실, 주차 시설
+
+    /*
+      공공데이터에서 제공하는 데이터
+
+      buildingFloors; //건물 층수
+      hallways; //복도
+      urinals;  //소변기
+      generalInformation; //일반 정보
+      door; // 문
+     */
 }
