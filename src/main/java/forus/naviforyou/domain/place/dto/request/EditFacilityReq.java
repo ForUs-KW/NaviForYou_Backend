61f768c94f5d550888d52bbf03abeabb9747ff88
jansoon <ckdals4600@naver.com>
2024-01-31 05:07:05 +0900

    :sparkles: Fix: Json 역직렬화 문제 해결 (#30) - @JsonCreator 사용한 생성자 작성으로 중첩 객체에 대한 파싱 문제 해결

--- src/main/java/forus/naviforyou/domain/place/dto/request/EditFacilityReq.java
+++ src/main/java/forus/naviforyou/domain/place/dto/request/EditFacilityReq.java
@@ -1,15 +1,27 @@
 package forus.naviforyou.domain.place.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import forus.naviforyou.global.common.collection.building.Location;
 import lombok.*;
 
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class EditFacilityReq {
-    private String roadAddress;
-    private Location location;
-    private String facilityName;
-    private Boolean edit;
+    private final String roadAddress;
+    private final Location location;
+    private final String facilityName;
+    private final Boolean edit;
+
+    @JsonCreator
+    public EditFacilityReq(
+            @JsonProperty("roadAddress") String roadAddress,
+            @JsonProperty("location") Location location,
+            @JsonProperty("facilityName") String facilityName,
+            @JsonProperty("edit") Boolean edit
+    ) {
+        this.roadAddress = roadAddress;
+        this.location = location;
+        this.facilityName = facilityName;
+        this.edit = edit;
+    }
 }
--- src/main/java/forus/naviforyou/domain/place/dto/request/ExistenceFacilityListReq.java
+++ src/main/java/forus/naviforyou/domain/place/dto/request/ExistenceFacilityListReq.java
@@ -1,15 +1,23 @@
 package forus.naviforyou.domain.place.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import forus.naviforyou.global.common.collection.building.Location;
 import lombok.*;
 
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ExistenceFacilityListReq {
-    private String buildingName;
-    private Location location;
-    private String roadAddress;
+    private final String buildingName;
+    private final Location location;
+    private final String roadAddress;
 
+    @JsonCreator
+    public ExistenceFacilityListReq(
+            @JsonProperty("buildingName") String buildingName,
+            @JsonProperty("location") Location location,
+            @JsonProperty("roadAddress") String roadAddress) {
+        this.buildingName = buildingName;
+        this.location = location;
+        this.roadAddress = roadAddress;
+    }
 }
