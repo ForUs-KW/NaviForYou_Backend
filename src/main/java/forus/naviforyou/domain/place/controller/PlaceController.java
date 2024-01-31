ce134150cff67a95e20b02575e30bd834c9e715b
jansoon <ckdals4600@naver.com>
2024-01-31 16:19:36 +0900

    :truck: Rename: 함수명, 클래스명 통일 (#30)

--- src/main/java/forus/naviforyou/domain/place/controller/PlaceController.java
+++ src/main/java/forus/naviforyou/domain/place/controller/PlaceController.java
@@ -2,7 +2,7 @@ package forus.naviforyou.domain.place.controller;
 
 import forus.naviforyou.domain.place.dto.request.ExistenceFacilityListReq;
 import forus.naviforyou.domain.place.dto.request.EditFacilityReq;
-import forus.naviforyou.domain.place.dto.response.BuildingFacilityListRes;
+import forus.naviforyou.domain.place.dto.response.ExistenceFacilityListRes;
 import forus.naviforyou.domain.place.service.PlaceService;
 import forus.naviforyou.global.common.BaseResponse;
 import forus.naviforyou.global.common.BaseResultRes;
@@ -21,7 +21,7 @@ public class PlaceController {
 
     @PostMapping("/convenientFacility")
     public ResponseEntity<?> getExistenceFacilityList(@RequestBody ExistenceFacilityListReq req){
-        BuildingFacilityListRes res = placeService.getExistenceFacilityList(req);
+        ExistenceFacilityListRes res = placeService.getExistenceFacilityList(req);
         return BaseResponse.ok(res);
     }
 
