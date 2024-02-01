16d3bf6fe6d8001f9dd4c3b202008b95c5a74fcf
jansoon <ckdals4600@naver.com>
2024-02-01 13:36:51 +0900

    :art: Style: 장애인 편의시설 수정 API Request 수정 (#30) - 요청 변수 추가: buildingName -@Getter 어노테이션 추가

--- src/main/java/forus/naviforyou/domain/place/dto/request/EditAccessibilityReq.java
+++ src/main/java/forus/naviforyou/domain/place/dto/request/EditAccessibilityReq.java
@@ -3,15 +3,19 @@ package forus.naviforyou.domain.place.dto.request;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import forus.naviforyou.global.common.collection.building.Location;
+import lombok.Getter;
 
-public record EditAccessibilityReq(String roadAddress, Location location, String facilityName, Boolean edit) {
+@Getter
+public record EditAccessibilityReq(String buildingName, String roadAddress, Location location, String facilityName, Boolean edit) {
     @JsonCreator
     public EditAccessibilityReq(
+            @JsonProperty("buildingName") String buildingName,
             @JsonProperty("roadAddress") String roadAddress,
             @JsonProperty("location") Location location,
             @JsonProperty("facilityName") String facilityName,
             @JsonProperty("edit") Boolean edit
     ) {
+        this.buildingName = buildingName;
         this.roadAddress = roadAddress;
         this.location = location;
         this.facilityName = facilityName;
