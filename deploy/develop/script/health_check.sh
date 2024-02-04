c2f5a51e17fbdeb94c45c70b2b6af3e2b92e848a
jansoon <ckdals4600@naver.com>
2024-02-05 02:02:37 +0900

    :rocket: Chore: 배포 스크립트 작성 (#39)

---
+++ deploy/develop/script/health_check.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# (1)
+url="http://localhost:8080/healthCheck"
+
+timeout=5
+online=false
+
+echo "Checking status of $url."
+
+# (2)
+loopCount=1
+while [ $loopCount -le 10 ]
+do
+  code=$(curl -sL --connect-timeout 20 --max-time 30 -w "%{http_code}\\n" "$url" -o /dev/null)
+  echo "try $loopCount => code: $code"
+  if [ "$code" = "200" ]; then
+    online=true
+    break
+  else
+    loopCount=$((loopCount + 1))
+    sleep $timeout
+  fi
+done
+
+# (3)
+if $online; then
+  echo "Monitor finished, website is online."
+  exit 0 # Success
+else
+  echo "Monitor failed, website seems to be down."
+  exit 1 # Failed
+fi
+출처: https://hello-startup.tistory.com/18 [Hello Startup:티스토리]
