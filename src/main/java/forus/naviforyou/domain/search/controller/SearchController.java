3e8dd08e980fecb5bc93831c2b8e41a96e006392
Jansoon <ckdals4600@naver.com>
2024-02-09 01:10:29 +0900

    :recycle: Refactor: T Map API에 따른 컨트롤러 및 Request DTO 수정 (#54)

--- src/main/java/forus/naviforyou/domain/search/controller/SearchController.java
+++ src/main/java/forus/naviforyou/domain/search/controller/SearchController.java
@@ -1,6 +1,5 @@
 package forus.naviforyou.domain.search.controller;
 
-import forus.naviforyou.domain.member.dto.request.SignUpReq;
 import forus.naviforyou.domain.search.dto.request.SearchReq;
 import forus.naviforyou.domain.search.dto.response.SearchRes;
 import forus.naviforyou.domain.search.service.SearchService;
@@ -18,10 +17,9 @@ public class SearchController {
     private final SearchService searchService;
 
     @ApiOperation(tags = "5. search", value = "키워드 검색", notes = "검색하여 결과를 출력합니다")
-
-    @GetMapping("/{name}")
-    public ResponseEntity<?> getSearch(@RequestBody SearchReq searchReq, @PathVariable String name){
-        SearchRes searchRes = searchService.searchInfo(searchReq , name);
+    @PostMapping
+    public ResponseEntity<?> getSearch(@RequestBody SearchReq searchReq){
+        SearchRes searchRes = searchService.searchInfo(searchReq);
         return BaseResponse.ok(searchRes);
     }
 
