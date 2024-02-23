e8d72b5b65a7a63d57e809282c6cd40529e69686
Jansoon <ckdals4600@naver.com>
2024-02-23 23:47:14 +0900

    :bug: Fix: jwt null 오류 추가 (#69)

--- src/main/java/forus/naviforyou/global/config/jwt/JwtFilter.java
+++ src/main/java/forus/naviforyou/global/config/jwt/JwtFilter.java
@@ -30,7 +30,7 @@ public class JwtFilter extends GenericFilterBean {
         String requestURI = httpServletRequest.getRequestURI();
         String jwt = resolveToken(httpServletRequest);
 
-        if (jwt != null && jwtTokenProvider.validateToken(jwt)) {
+        if (jwtTokenProvider.validateToken(jwt)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
             log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
